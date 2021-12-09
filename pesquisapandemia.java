c95a4115bc166e3410b87d04897cd1f37c973d23
Ailton Dos Santos <64874024+ailton-santos@users.noreply.github.com>
2021-12-08 21:04:38 -0300

    Add files via upload

---
+++ pesquisapandemia.java
@@ -0,0 +1,21 @@
+package pesquisapandemia;
+import javax.swing.JOptionPane; 
+import java.util.Scanner;
+
+public class pesquisapandemia 
+		{
+
+	public static void main(String[] args) 
+		{
+		// TODO Auto-generated method stub
+		int i;
+		for (i=0; i<51; i++)
+		{
+			JOptionPane.showMessageDialog(null, i);
+		}
+		}
+			
+
+		}
+
+
